a7323630c295f750b156a5b79f46c622233f884b
Morgan <143060948+MorganShae@users.noreply.github.com>
2023-10-18 03:24:53 +0000

    Remove version number

--- src/tutorial.cxx
+++ src/tutorial.cxx
@@ -3,19 +3,10 @@
 #include <string>
 
 #include "MathFunctions/MathFunctions.h"
-#include "TutorialConfig.h.in"
 
 int main(int argc, char* argv[])
 {
-  if (argc < 2) {
-    // report version
-    std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
-              << Tutorial_VERSION_MINOR << std::endl;
-    std::cout << "Usage: " << argv[0] << " number" << std::endl;
-    return 1;
-  }
-
-  // convert input to double
+  // Convert input to double
   const double inputValue = std::stod(argv[1]);
 
   const double outputValue = mathfunctions::sqrt(inputValue);
