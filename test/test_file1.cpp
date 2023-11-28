95cddf7b95fbd71da83bb1ebc35604f0942e0c42
Morgan <143060948+MorganShae@users.noreply.github.com>
2023-11-29 09:37:35 +1030

    Update test_file1.cpp

--- test/test_file1.cpp
+++ test/test_file1.cpp
@@ -1,7 +1,1 @@
-#include <boost/test/unit_test.hpp>
 #include "MathFunctions/MathFunctions.h"
-
-BOOST_AUTO_TEST_CASE(test_sqrt) {
-    double result = mathfunctions::sqrt(4.0);
-    BOOST_CHECK_CLOSE(result, 2.0, 0.0001); // Check if result is close to 2.0
-}
