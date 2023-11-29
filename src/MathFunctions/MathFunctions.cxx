65cc87e44bea13cd32d1ccbb2bb49144e07109af
Morgan <143060948+MorganShae@users.noreply.github.com>
2023-11-29 15:55:50 +1030

    Update MathFunctions.cxx

--- src/MathFunctions/MathFunctions.cxx
+++ src/MathFunctions/MathFunctions.cxx
@@ -3,12 +3,6 @@
 #include "MathFunctions.h"
 #include <cmath>
 
-#ifdef MY_LIBRARY_EXPORTS
-    #define MY_LIBRARY_API __declspec(dllexport)
-#else
-    #define MY_LIBRARY_API __declspec(dllimport)
-#endif
-
 namespace mathfunctions {
     MY_LIBRARY_API double sqrt(double x) {
         return std::sqrt(x);
