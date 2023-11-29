47ff61670b1ad54eb780ba6febcff2270b090fb4
Morgan <143060948+MorganShae@users.noreply.github.com>
2023-11-29 15:53:50 +1030

    Update MathFunctions.h

--- src/MathFunctions/MathFunctions.h
+++ src/MathFunctions/MathFunctions.h
@@ -2,6 +2,18 @@
 
 #pragma once
 
+#if defined(_WIN32)
+    // For Windows
+    #ifdef MY_LIBRARY_EXPORTS
+        #define MY_LIBRARY_API __declspec(dllexport)
+    #else
+        #define MY_LIBRARY_API __declspec(dllimport)
+    #endif
+#else
+    // For Linux and other platforms
+    #define MY_LIBRARY_API __attribute__((visibility("default")))
+#endif
+
 namespace mathfunctions {
-    __declspec(dllimport) double sqrt(double x);
+    MY_LIBRARY_API double sqrt(double x);
 }
