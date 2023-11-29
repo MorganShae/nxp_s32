55ba2c29f38a5d9bdc90f54c5b3635b3a897ca58
Morgan <143060948+MorganShae@users.noreply.github.com>
2023-11-29 15:48:20 +1030

    Update MathFunctions.cxx

--- src/MathFunctions/MathFunctions.cxx
+++ src/MathFunctions/MathFunctions.cxx
@@ -1,20 +1,10 @@
+// src/MathFunctions/MathFunctions.cxx
 
 #include "MathFunctions.h"
-
 #include <cmath>
 
-#ifdef USE_MYMATH
-#  include "mysqrt.h"
-#endif
-
 namespace mathfunctions {
-double sqrt(double x)
-{
-// which square root function should we use?
-#ifdef USE_MYMATH
-  return detail::mysqrt(x);
-#else
-  return std::sqrt(x);
-#endif
-}
+    __declspec(dllexport) double sqrt(double x) {
+        return std::sqrt(x);
+    }
 }
