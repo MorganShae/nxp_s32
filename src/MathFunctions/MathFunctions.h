db5e0db9c9757f8e43a9ddd5c67a0a1c41ad5b10
Morgan <143060948+MorganShae@users.noreply.github.com>
2023-11-29 15:49:53 +1030

    Update MathFunctions.h

--- src/MathFunctions/MathFunctions.h
+++ src/MathFunctions/MathFunctions.h
@@ -1,14 +1,7 @@
+// src/MathFunctions/MathFunctions.h
 
-#if defined(_WIN32)
-#  if defined(EXPORTING_MYMATH)
-#    define DECLSPEC __declspec(dllexport)
-#  else
-#    define DECLSPEC __declspec(dllimport)
-#  endif
-#else // non windows
-#  define DECLSPEC
-#endif
+#pragma once
 
 namespace mathfunctions {
-double DECLSPEC sqrt(double x);
+    __declspec(dllimport) double sqrt(double x);
 }
