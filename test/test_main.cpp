6819d103e58500533718786d547e1cc8417806f4
Morgan <143060948+MorganShae@users.noreply.github.com>
2023-11-30 12:57:21 +1030

    Update test_main.cpp

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,8 +1,33 @@
-// test/test_main.cpp
-
 #include <iostream>
 
+// Define your test functions here
+
+void testAddition() {
+    int result = 2 + 2;
+    if (result == 4) {
+        std::cout << "Test Addition: Passed" << std::endl;
+    } else {
+        std::cerr << "Test Addition: Failed" << std::endl;
+    }
+}
+
+void testMultiplication() {
+    int result = 2 * 3;
+    if (result == 6) {
+        std::cout << "Test Multiplication: Passed" << std::endl;
+    } else {
+        std::cerr << "Test Multiplication: Failed" << std::endl;
+    }
+}
+
+// ...
+
 int main() {
-    std::cout << "Running tests...\n";
+    // Run your test functions
+    testAddition();
+    testMultiplication();
+
+    // ...
+
     return 0;
 }
